f1b4c4804fdb79fe1aa61037909ec62e0bda92ac
syshopofficial1-cmyk <syshopofficial1@gmail.com>
2026-03-28 13:55:47 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// js/config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMEAsJI4DhnLNP2G6F8e56KxfoFassci0", // ⚠️ เปลี่ยนเป็นคีย์ใหม่!
+  authDomain: "syshop-official.firebaseapp.com",
+  projectId: "syshop-official",
+  storageBucket: "syshop-official.firebasestorage.app",
+  messagingSenderId: "700567443749",
+  appId: "1:700567443749:web:06553838223d67d69362cc",
+  measurementId: "G-0CZCMD0KHH"
+};
+
+export const LINE_OA_URL = "https://line.me/ti/p/@YOUR_LINE_ID"; // แก้เป็นลิงก์ไลน์ของคุณ
