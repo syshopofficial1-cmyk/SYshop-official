8dfa6bc3d821ceab07c84fc4f142aed2304c8f8e
syshopofficial1-cmyk <syshopofficial1@gmail.com>
2026-03-28 19:02:00 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,16 @@
 // js/config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyDMEAsJI4DhnLNP2G6F8e56KxfoFassci0", // ⚠️ เปลี่ยนเป็นคีย์ใหม่!
+  apiKey: "AIzaSyDMEAsJI4DhnLNP2G6F8e56KxfoFassci0",
   authDomain: "syshop-official.firebaseapp.com",
   projectId: "syshop-official",
-  storageBucket: "syshop-official.firebasestorage.app",
+  storageBucket: "syshop-official.appspot.com",
   messagingSenderId: "700567443749",
-  appId: "1:700567443749:web:06553838223d67d69362cc",
-  measurementId: "G-0CZCMD0KHH"
+  appId: "1:700567443749:web:06553838223d67d69362cc"
 };
 
-export const LINE_OA_URL = "https://line.me/ti/p/@YOUR_LINE_ID"; // แก้เป็นลิงก์ไลน์ของคุณ
+export const cloudinaryConfig = {
+  cloudName: "dofazg7gg",
+  uploadPreset: "syshop_preset"  // ต้องสร้างใน Cloudinary ก่อน!
+};
+
+export const LINE_OA_URL = "https://line.me/ti/p/@YOUR_LINE_ID";
